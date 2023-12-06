e50d6a7e837ae76eaf55a102696f680860b94c72
Siddhant <s1d.official011001011@gmail.com>
2023-12-06 08:28:21 +0000

    asdsad

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App = () => {
       const formData = new FormData();
       formData.append("image", file);
       const response = await axios.post(
-        "https://api-sketch.gokapturehub.com/pencil_sketch",
+        "https://api-sketch.gokapturehub.com/pencil_sketch?blur_simga=10",
         formData,
         {
           headers: {
